3ec39759d65cba2e689016b21f498398add8033b
WestleyArgentum <WestleyArgentum@gmail.com>
2014-07-19 21:46:52 -0700

    tabs => spaces

--- src/influence-explorer.jl
+++ src/influence-explorer.jl
@@ -6,12 +6,14 @@ global INFLUENCE_EXPLORER_API = URI("http://transparencydata.com/")
 # -------
 
 function entity_search(auth::String, search_str; entity_type = nothing, options...)
-	args = {
-		"search" => search_str
-	}
-	entity_type != nothing && (args["type"] = entity_type)
+    args = {
+        "search" => search_str
+    }
+    entity_type != nothing && (args["type"] = entity_type)
 
-	sunlight_get(auth, INFLUENCE_EXPLORER_API, "/api/1.0/entities.json", args; options...)
+    sunlight_get(auth, INFLUENCE_EXPLORER_API, "/api/1.0/entities.json", args; options...)
 end
 
 entity_search(search_str; auth = "", options...) = entity_search(auth, search_str; options...)
+
+# -------
--- src/utils.jl
+++ src/utils.jl
@@ -2,14 +2,14 @@
 sunlight_get(auth::String, api::URI, path, args; options...) = sunlight_get(auth, URI(api; path = path), args; options...)
 
 function sunlight_get(auth::String, route::URI, args; query = Dict(), options...)
-	query["apikey"] = auth
-	merge!(query, args)
+    query["apikey"] = auth
+    merge!(query, args)
 
-	r = get(route; query = query, options...)
+    r = get(route; query = query, options...)
 
-	handle_error(r)
+    handle_error(r)
 
-	JSON.parse(r.data)
+    JSON.parse(r.data)
 end
 
 # -------
