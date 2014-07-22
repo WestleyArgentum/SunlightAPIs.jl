bf2ea4d78133a2c0c02a451c05f9ecf0f1894454
WestleyArgentum <WestleyArgentum@gmail.com>
2014-07-22 15:19:04 -0400

    function generators contain api-specific references, should be defined in the api files

--- src/influence-explorer.jl
+++ src/influence-explorer.jl
@@ -1,7 +1,72 @@
 
-# For more information, see:  http://sunlightlabs.github.io/datacommons
-
+# http://sunlightlabs.github.io/datacommons
 global INFLUENCE_EXPLORER_API = URI("http://transparencydata.com/")
+
+# -------
+
+macro sunlight_method__id_limit_cycle(method_name, method_route)
+    quote
+        function $(esc(symbol(method_name)))(auth::String, entity_id; limit = nothing, cycle = nothing, options...)
+            method_route = $method_route
+
+            args = Dict()
+            limit != nothing && (args["limit"] = limit)
+            cycle != nothing && (args["cycle"] = cycle)
+
+            sunlight_get(auth, INFLUENCE_EXPLORER_API, "/api/1.0/aggregates/pol/$entity_id/$method_route", args; options...)
+        end
+
+        $(esc(symbol(method_name)))(entity_id; auth = "", options...) = $(esc(symbol(method_name)))(auth, entity_id; options...)
+    end
+end
+
+# -------
+
+macro sunlight_method__id_cycle(method_name, method_route)
+    quote
+        function $(esc(symbol(method_name)))(auth::String, entity_id; cycle = nothing, options...)
+            method_route = $method_route
+
+            args = Dict()
+            cycle != nothing && (args["cycle"] = cycle)
+
+            sunlight_get(auth, INFLUENCE_EXPLORER_API, "/api/1.0/aggregates/pol/$entity_id/$method_route", args; options...)
+        end
+
+        $(esc(symbol(method_name)))(entity_id; auth = "", options...) = $(esc(symbol(method_name)))(auth, entity_id; options...)
+    end
+end
+
+# -------
+
+macro sunlight_method__id(method_name, method_route)
+    quote
+        function $(esc(symbol(method_name)))(auth::String, entity_id; options...)
+            method_route = $method_route
+            sunlight_get(auth, INFLUENCE_EXPLORER_API, "/api/1.0/aggregates/pol/$entity_id/$method_route", Dict(); options...)
+        end
+
+        $(esc(symbol(method_name)))(entity_id; auth = "", options...) = $(esc(symbol(method_name)))(auth, entity_id; options...)
+    end
+end
+
+# -------
+
+@sunlight_method__id_limit_cycle "top_contributors" "contributors.json"
+
+@sunlight_method__id_limit_cycle "top_industries" "contributors/industries.json"
+
+@sunlight_method__id_cycle "unknown_industries" "contributors/industries_unknown.json"
+
+@sunlight_method__id_limit_cycle "top_sectors" "contributors/sectors.json"
+
+@sunlight_method__id_cycle "local_breakdown" "contributors/local_breakdown.json"
+
+@sunlight_method__id_limit_cycle "contributor_breakdown" "contributors/type_breakdown.json"
+
+@sunlight_method__id_limit_cycle "fec_summary" "fec_summary.json"
+
+@sunlight_method__id "fec_independent_expenditures" "fec_indexp.json"
 
 # -------
 
@@ -37,23 +102,3 @@ function top_politicians(auth::String; limit = 16, cycle = nothing, options...)
 end
 
 top_politicians(; auth = "", options...) = top_politicians(auth; options...)
-
-# -------
-
-@sunlight_method__id_limit_cycle "top_contributors" "contributors.json"
-
-@sunlight_method__id_limit_cycle "top_industries" "contributors/industries.json"
-
-@sunlight_method__id_cycle "unknown_industries" "contributors/industries_unknown.json"
-
-@sunlight_method__id_limit_cycle "top_sectors" "contributors/sectors.json"
-
-@sunlight_method__id_cycle "local_breakdown" "contributors/local_breakdown.json"
-
-@sunlight_method__id_limit_cycle "contributor_breakdown" "contributors/type_breakdown.json"
-
-@sunlight_method__id_limit_cycle "fec_summary" "fec_summary.json"
-
-@sunlight_method__id "fec_independent_expenditures" "fec_indexp.json"
-
-# -------
